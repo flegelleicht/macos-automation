901bc2a88cd8faa9ca439346f8548cc71b52f192
Erik Hebisch <flegelleicht@gmail.com>
2018-09-21 12:10:17 +0200

    Changed from console output to writing a file in TaskPaper format

--- collect-safari-urls.js
+++ collect-safari-urls.js
@@ -1,22 +1,60 @@
 #! /usr/bin/env osascript -l JavaScript
 
-function run() {
-  let app = Application.currentApplication();
-  app.includeStandardAdditions = true;
+let app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+function prependToStandardHistoryFile(str) {
+  let standardHistoryFile = '~/.safari-sessions';
   
+  try {
+    let previousContent = '';
+    
+    if($.NSFileManager.defaultManager.fileExistsAtPath(standardHistoryFile)) {
+      let previousContent = $.NSString.stringWithContentsOfFileEncodingError(standardHistoryFile, $.NSUTF8StringEncoding, null);
+    }
+    
+    let newContent =  `${str}${previousContent}`;
+    
+    content = $.NSString.alloc.initWithUTF8String(newContent);
+    content.writeToFileAtomicallyEncodingError(standardHistoryFile, true, $.NSUTF8StringEncoding, null);
+        
+    return true;
+    
+  } catch (error) {
+    console.log(`Couldn't write file due to ${error}`);
+    
+    try { app.closeAccess(standardHistoryFile); } 
+    catch (error) { console.log(`Couldn't close file due to ${error}'`); }
+    return false;
+  }
+}
+
+function run() {
   let safari = Application('Safari');
-  let windows = safari.windows
-  for(let i = 0; i < windows.length; ++i) {
-    let w = windows[i];
-    let tabs = w.tabs;
-    console.log(`\n# ${i+1} – ${tabs.length} tabs`);
-    for(let j = 0; j < tabs.length; ++j) {
-      let t = tabs[j];
-      if(t.url() == null) {
-        console.log(`1. ${t.name()}`)
-      } else {
-        console.log(`1. [${t.name()}](${t.url()})`);
+  let windows = safari.windows;
+  let result = '';
+  
+  if( windows && windows.length > 0) {
+    result += `Session ${(new Date()).toLocaleString('de-DE', { timeZone: 'UTC'})}:\n`;
+    
+    for(let i = 0; i < windows.length; ++i) {
+      let w = windows[i];
+      let tabs = w.tabs;
+    
+      result += `\t${i+1} – ${tabs.length} tabs:\n`;
+
+      for(let j = 0; j < tabs.length; ++j) {
+        let t = tabs[j];
+        if(t.url() == null) {
+          result += `\t\t- ${t.name()}\n`
+        } else {
+          result += `\t\t- ${t.name()}\n\t\t\t${t.url()}\n`;
+        }
       }
     }
+  }
+  
+  if(result !== '') {
+    prependToStandardHistoryFile(result);
   }
 }
